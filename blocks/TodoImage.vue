72ac520e61565536dc7d92c7afc0b8c514ba9bd8
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2024-03-01 16:58:32 -0300

    📦 chore(TodoImage.vue): add TodoImage component to handle image upload functionality
    This commit adds a new component called TodoImage.vue. This component handles the functionality of uploading and displaying an image. It includes the following features:
    
    - The component imports the Icon component from the '@iconify/vue' library.
    - The component defines an emits property with a 'saveImageId' event.
    - The component initializes a reactive variable called 'imageUrl' with a default image URL.
    - The component defines a function called 'setImageUrl' that updates the 'imageUrl' variable with a new URL.
    - The component defines a function called 'saveImageOnLocalStorage' that saves the image URL to local storage and emits the 'saveImageId' event with a generated image ID.
    - The component defines a function called 'updateOnChange' that handles the file input change event. It reads the selected file using the FileReader API, converts it to a data URL, updates the 'imageUrl' variable, and saves the image URL to local storage.
    - The component includes a template with a container div that displays the uploaded image. It also includes an input element for selecting a file, an Icon component for displaying a file upload icon, and an img element for displaying the uploaded image.
    
    No newline at end of file.

---
+++ blocks/TodoImage.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+
+const emits = defineEmits(['saveImageId']);
+
+const imageUrl = ref<string>('https://cdn.vectorstock.com/i/preview-1x/65/30/default-image-icon-missing-picture-page-vector-40546530.jpg');
+
+const setImageUrl  = (url: string) => {
+  imageUrl.value = url;
+}
+
+const saveImageOnLocalStorage = (url: string) => {
+  const imageId = generateId();
+  saveItemOnLocalStorage(imageId, url);
+  emits('saveImageId', imageId);
+}
+
+const updateOnChange = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  const files = target.files;
+  const fileReader = new FileReader();
+
+  if (!files) return;
+
+  fileReader.readAsDataURL(files[0]);
+
+  fileReader.addEventListener('load', () => {
+    const url = String(fileReader.result)
+
+    if(!url) return
+
+    setImageUrl(url);
+    saveImageOnLocalStorage(url);
+  })
+};
+
+</script>
+<template>
+  <div
+    class="relative flex justify-center items-center w-full h-full max-w-[17rem] max-h-[17rem] overflow-hidden rounded-xl relative border shadow group"
+    title="Click to upload an image"
+  >
+    <input
+      type="file"
+      class="absolute inset-0 w-full h-full cursor-pointer opacity-0 z-[99]"
+      accept=".png,.jpg,.jpeg"
+      @change="updateOnChange"
+    />
+    <Icon
+      icon="mdi:file-plus-outline"
+      class="absolute inset-0 w-full h-full m-auto text-gray-500 opacity-0 group-hover:opacity-30"
+    />
+    <img
+      :src="imageUrl"
+      loading="lazy"
+      class="object-cover w-full h-full"
+      alt="image"
+    />
+  </div>
+</template>
