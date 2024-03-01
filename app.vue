f49b1329dd61f0513c741f3fcee0e031369f6841
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2024-03-01 17:38:35 -0300

    🎨 style(app.vue): update CSS classes to improve layout and responsiveness

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="prose w-screen h-screen px-2">
+  <div class="prose px-2 overflow-hidden overflow-y-auto max-w-[1920px] mx-auto">
     <NuxtLayout />
   </div>
 </template>
