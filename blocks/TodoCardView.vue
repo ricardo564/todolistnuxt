79547604e91d1ab02f653a2838379b6152928d47
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2024-03-01 17:44:39 -0300

    🎉 feat(TodoCardView.vue): add TodoCardView component to display a todo list item with image, title, description, and action buttons
    This commit adds a new file `TodoCardView.vue` which contains the template for rendering a todo list item. The component includes a container with a fixed width and rounded corners, a section for displaying an image, a section for displaying the title and description, and a section for action buttons (delete, edit, and add).
    
    The template uses Tailwind CSS classes for styling and includes dynamic content for the title and description. The title is truncated if it exceeds a certain length, and the description is displayed with a paragraph element.
    
    No newline at the end of the file.

---
+++ blocks/TodoCardView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-col w-[17rem] border rounded-xl shadow-md hover:shadow relative">
+    <div class="h-[15rem]">
+      <TodoImage />
+    </div>
+    <div class="w-[16rem] max-h-[15rem] overflow-hidden overflow-y-auto mx-auto p-5">
+      <p class="text-2xl font-bold mx-auto h-[2rem] max-w-[15rem] overflow-hidden truncate text-ellipsis">
+        This is a todo list Item
+      </p>
+      <p>
+        Description of the todo list
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur amet ratione ex, dolorum repellat ullam fuga vero exercitationem quia impedit aut nihil, perferendis voluptatem dignissimos corporis aliquid sapiente optio dolores.
+      </p>
+    </div>
+    <div class="flex">
+      <button class="w-full p-2 bg-red-500 text-white border-white border">
+        Delet
+      </button>
+      <button class="w-full p-2 bg-orange-500 text-white border-white border">
+        Edit
+      </button>
+      <button class="w-full p-2 bg-green-500 text-white border-white border">
+        Add
+      </button>
+    </div>
+  </div>
+</template>
