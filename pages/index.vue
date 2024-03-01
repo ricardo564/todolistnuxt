ddfe0280bcd17c68c4a6bdc221ce1860cf461591
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2024-03-01 16:59:01 -0300

    🎨 style(index.vue): add TodoImage component to the index page for better visual representation of the todo list

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import TodoImage from '@/blocks/todoImage.vue';
+
+</script>
 <template>
     <h1 class="mx-auto text-xl">
         Todo - List
     </h1>
+    <TodoImage />
 </template>
