8333d1460d007cab9f6d520911b365e1e7ac0956
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2024-03-01 21:04:34 -0300

    🔧 chore(index.vue): refactor code to use Composition API and improve readability 🐛 fix(index.vue): add missing newline at the end of the file for consistency

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,27 @@
+<script setup lang="ts">
+const toDoList = ref<any[]>([]);
+
+
+const setToDoList = () => {
+  const todoList = JSON.parse(getItemFromLocalStorage('todo-list'));
+  toDoList.value = todoList;
+}
+
+onMounted(() => {
+  setToDoList();
+})
+
+</script>
 <template>
     <h1 class="mx-auto text-3xl">
         Todo - List
     </h1>
     <div class="flex gap-4 flex-wrap group">
-      <TodoCardCreate />
-      <TodoCardView />
+      <TodoCardCreate
+        @refreshTodoList="setToDoList"
+      />
+      <TodoCardView
+        :toDoList="toDoList.reverse()"
+      />
     </div>
 </template>
